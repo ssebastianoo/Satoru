e784ea7ead71704213466a6851545aa0ffacd3c7
ssebastianoo <sebastianogirotto05@gmail.com>
2020-12-07 10:09:50 +0100

    eval

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import discord, config
+import discord, config, os
 from discord.ext import commands 
 from ext.bot import Bot
+
+os.environ["JISHAKU_NO_UNDERSCORE"] = "True"
+os.environ["JISHAKU_NO_DM_TRACEBACK"] = "True" 
+os.environ["JISHAKU_HIDE"] = "True"
 
 bot = Bot(command_prefix=commands.when_mentioned_or("e?"))
 bot.run(config.tokens.bot)
