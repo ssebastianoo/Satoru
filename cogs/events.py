0d2358ce9f02d732e043dbf5f0a15f1ce83c91bf
ssebastianoo <sebastianogirotto05@gmail.com>
2020-12-14 23:54:46 +0100

    accepting only images

--- cogs/events.py
+++ cogs/events.py
@@ -1,6 +1,5 @@
 import discord
 from discord.ext import commands
-
 class Events(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
--- cogs/misc.py
+++ cogs/misc.py
@@ -121,7 +121,7 @@ class Misc(commands.Cog):
         await cs.close()
 
         memes = memes["data"]["children"]
-        memes = [post["data"] for post in memes if not post["data"]["is_self"]]
+        memes = [post["data"] for post in memes if not post["data"]["is_self"] and ".png" in post["data"]["url_overridden_by_dest"] or ".jpg" in post["data"]["url_overridden_by_dest"] or ".gif" in post["data"]["url_overridden_by_dest"] or ".jpeg" in post["data"]["url_overridden_by_dest"]]
         meme = random.choice(memes)
 
         url = meme["url_overridden_by_dest"]
